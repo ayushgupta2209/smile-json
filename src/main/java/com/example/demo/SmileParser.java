3dde651dc86413a64197509d6a54a2080b0ac213
Ayush <ayushguptadtu@gmail.com>
2019-02-04 13:10:21 +0530

    Fixing the issue to read json using standard mapper

--- src/main/java/com/example/demo/SmileParser.java
+++ src/main/java/com/example/demo/SmileParser.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.smile.SmileFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -15,12 +17,14 @@ import java.io.IOException;
  */
 
 @Slf4j
+@Component
 public class SmileParser {
-  final ObjectMapper mapper = new ObjectMapper(new SmileFactory());
+  final ObjectMapper smileMapper = new ObjectMapper(new SmileFactory());
+  final ObjectMapper mapper = new ObjectMapper();
 
   public byte[] jsonToSmile(String jsonValue) throws JsonProcessingException {
     try {
-       return mapper.writeValueAsBytes(jsonValue);
+       return smileMapper.writeValueAsBytes(jsonValue);
     } catch (JsonProcessingException e) {
       log.error("Some error occurred in json parsing", e);
       throw e;
@@ -30,7 +34,7 @@ public class SmileParser {
   public byte[] jsonToSmile(File jsonFile) throws IOException {
     try {
       JsonNode jsonNode = mapper.readValue(jsonFile, JsonNode.class);
-      return mapper.writeValueAsBytes(jsonNode);
+      return smileMapper.writeValueAsBytes(jsonNode);
     } catch (IOException e) {
       log.error("Some error occurred in json parsing", e);
       throw e;
@@ -39,7 +43,7 @@ public class SmileParser {
 
   public JsonNode smileToJSON(byte[] smileBytes) throws IOException {
     try {
-      return mapper.readValue(smileBytes, JsonNode.class);
+      return smileMapper.readValue(smileBytes, JsonNode.class);
     } catch (IOException e) {
       log.error("Some error occurred in smile parsing", e);
       throw e;
@@ -48,10 +52,18 @@ public class SmileParser {
 
   public JsonNode smileToJSON(File smileFile) throws IOException {
     try {
-      return mapper.readValue(smileFile, JsonNode.class);
+      return smileMapper.readValue(smileFile, JsonNode.class);
     } catch (IOException e) {
       log.error("Some error occurred in smile parsing", e);
       throw e;
+    }
+  }
+
+  public void writeToFile(byte[] bytes, String fileDest) {
+    try (FileOutputStream fileOuputStream = new FileOutputStream(fileDest)) {
+      fileOuputStream.write(bytes);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
