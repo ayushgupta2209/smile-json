e68c0e3e8017735910330bfc5c24c3a004829df4
Ayush <ayushguptadtu@gmail.com>
2019-02-04 13:10:49 +0530

    Testing out conversion of 100Kb json file to smile(sml) file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,7 +15,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+  private static String resourceFolderPath = "./src/main/resources/";
+
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+    SmileParser smileParser = applicationContext.getBean(SmileParser.class);
+
+    try {
+      File directory = new File(resourceFolderPath + "Sample_100KB.json");
+      byte[] smileBytes = smileParser.jsonToSmile(directory);
+      smileParser.writeToFile(smileBytes, resourceFolderPath + "Sample_100KB_json_converted.sml");
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    System.exit(0);
   }
 }
